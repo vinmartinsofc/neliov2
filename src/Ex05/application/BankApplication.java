cf71d4c4eb098eb0a1bc0cfd473dee88843e8789
Vin Martins <marcosviniciossl3@gmail.com>
2026-06-01 20:16:09 -0300

    update: day one finished

--- src/Ex05/application/BankApplication.java
+++ src/Ex05/application/BankApplication.java
@@ -10,69 +10,73 @@ public class BankApplication {
         Account account;
         int option = -5;
 
+        try {
 
-        System.out.print("Enter the account number: ");
-        int accountNumber = scanner.nextInt();
-        scanner.nextLine();
+            System.out.print("Enter the account number: ");
+            int accountNumber = Integer.parseInt(scanner.nextLine());
 
-        System.out.print("Enter the holder: ");
-        String accountHolder = scanner.nextLine();
-        System.out.print("Initial deposit? (y/n): ");
-        String balanceOption = scanner.nextLine();
+            System.out.print("Enter the holder: ");
+            String accountHolder = scanner.nextLine();
 
-        if (balanceOption.equalsIgnoreCase("n")) {
-            account = new Account(accountNumber, accountHolder);
-        } else {
-            System.out.print("Enter the deposit: ");
-            double initialDeposit = scanner.nextDouble();
-            account = new Account(accountNumber, accountHolder, initialDeposit);
-        }
+            System.out.print("Initial deposit? (y/n): ");
+            String balanceOption = scanner.nextLine();
+
+            if (balanceOption.equalsIgnoreCase("n")) {
+                account = new Account(accountNumber, accountHolder);
+            } else {
+                System.out.print("Enter the deposit: ");
+                double initialDeposit = Double.parseDouble(scanner.nextLine());
+                account = new Account(accountNumber, accountHolder, initialDeposit);
+            }
 
 
-        while (option != 0) {
-            account.interactiveMenu();
+            while (option != 0) {
+                account.interactiveMenu();
 
-            option = scanner.nextInt();
+                option = Integer.parseInt(scanner.nextLine());
 
-            switch (option) {
+                switch (option) {
 
-                case 1: {
-                    System.out.print("Enter the amount: ");
-                    double depositAmount = scanner.nextDouble();
+                    case 1: {
+                        System.out.print("Enter the amount: ");
+                        double depositAmount = Double.parseDouble(scanner.nextLine());
 
-                    account.deposit(depositAmount);
-                    break;
+                        account.deposit(depositAmount);
+                        break;
+                    }
+
+                    case 2: {
+                        System.out.print("Enter the amount: ");
+                        double withdrawAmount = Double.parseDouble(scanner.nextLine());
+
+                        account.withdraw(withdrawAmount);
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("Enter the new owner: ");
+                        String updatedAccountHolder = scanner.nextLine();
+                        account.setHolder(updatedAccountHolder);
+                        break;
+                    }
+
+                    case 4: {
+                        account.accountStatus();
+                        break;
+                    }
+
+                    case 0:
+                        System.out.println("Leaving...");
+                        break;
+
+                    default:
+                        System.out.println("Invalid option");
+                        break;
                 }
-
-                case 2: {
-                    System.out.print("Enter the amount: ");
-                    double withdrawAmount = scanner.nextDouble();
-
-                    account.withdraw(withdrawAmount);
-                    break;
-                }
-                case 3: {
-                    scanner.nextLine();
-
-                    System.out.println("Enter the new owner: ");
-                    String updatedAccountHolder = scanner.nextLine();
-                    account.setHolder(updatedAccountHolder);
-                    break;
-                }
-
-                case 4: {
-                    account.accountStatus();
-                    break;
-                }
-
-                case 0: System.out.println("Leaving...");
-
-                default:
-                    System.out.println("Invalid option");
-                    break;
 
             }
 
+        } catch (NumberFormatException | NullPointerException e) {
+            System.out.println("Invalid Input");
         }
 
         scanner.close();
